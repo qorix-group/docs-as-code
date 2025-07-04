1a60859c3ab9b33c73671b0e34c7a9a1f90abb8f
Dan Calavrezo <dan.calavrezo.ext@qorix.ai>
2025-07-04 18:22:47 +0300

    test

--- src/extensions/score_source_code_linker/parse_source_files.py
+++ src/extensions/score_source_code_linker/parse_source_files.py
@@ -78,15 +78,8 @@ def find_git_root():
     This is copied from 'find_runfiles' as the import does not work for some reason.
     This should be fixed.
     """
-    git_root = Path.cwd().resolve()
-    while not (git_root / ".git").exists():
-        git_root = git_root.parent
-        if git_root == Path("/"):
-            sys.exit(
-                "Could not find git root. Please run this script from the "
-                "root of the repository."
-            )
-    return git_root
+
+    return Path.cwd().resolve()
 
 
 def get_git_hash(file_path: str) -> str:
